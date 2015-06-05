1fb99708331abf76d2d1a0de0dd6526369215651
XuJian1252878 <1252878xujian@tongji.edu.cn>
2015-06-06 00:29:26 +0800

    init post

---
+++ BBS.Service/BusinessServices/BoardService.cs
@@ -0,0 +1,40 @@
+﻿using BBS.Domain.Abstract;
+using BBS.Domain.Entities;
+using BBS.Service.BusinessInterfaces;
+using BBS.Service.BusinessServices.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBS.Service.BusinessServices
+{
+    public class BoardService: BaseService, IBoardService
+    {
+        private IBoardRepository boardRepository;
+
+        public BoardService()
+        {
+            boardRepository = LoadRepository<IBoardRepository>();
+        }
+
+        public IEnumerable<Board> GetRootBoards()
+        {
+            IEnumerable<Board> rootBoards = boardRepository.Find(board => board.ParentBoardID == null);
+            return rootBoards;
+        }
+
+        public IEnumerable<Board> GetChildBoards(int parentBoardID)
+        {
+            IEnumerable<Board> childBoards = boardRepository.Find(board => board.ParentBoardID == parentBoardID);
+            return childBoards;
+        }
+
+        public Board GetBoardByID(int boardID)
+        {
+            Board targetBoard = boardRepository.Find(board => board.ID == boardID).FirstOrDefault();
+            return targetBoard;
+        }
+    }
+}
---
+++ BBS.Service/BusinessServices/PostService.cs
@@ -0,0 +1,33 @@
+﻿using BBS.Domain.Abstract;
+using BBS.Domain.Entities;
+using BBS.Service.BusinessInterfaces;
+using BBS.Service.BusinessServices.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBS.Service.BusinessServices
+{
+    public class PostService: BaseService, IPostService
+    {
+        private IPostRepository postRepository = null;
+        public PostService()
+        {
+            this.postRepository = LoadRepository<IPostRepository>();
+        }
+
+        public IEnumerable<Post> GetPostsByBoard(int boardID)
+        {
+            IEnumerable<Post> targetPosts = postRepository.Find(post => post.BoardID == boardID);
+            return targetPosts;
+        }
+
+        public Post GetPostByID(int postID)
+        {
+            Post targetPost = postRepository.Find(post => post.ID == postID).FirstOrDefault();
+            return targetPost;
+        }
+    }
+}
